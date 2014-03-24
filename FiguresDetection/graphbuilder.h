2105adaf6437897a7f10b1b8011cfc17537fac88
Denis Polyakov <den1s.polyakov1@gmail.com>
2014-03-24 12:57:37 +0200

    Update graphbuilder.h

--- FiguresDetection/graphbuilder.h
+++ FiguresDetection/graphbuilder.h
@@ -9,6 +9,7 @@ public:
 	//returns all the edges of graph
 	std::set<Line> getEdges(std::vector<Point> &points);
 private:
+	//checks if two lines intersect
 	static bool intersects(const Point &s1, const Point &e1,
 		const Point &s2, const Point &e2, Point &result);
 	static void addLine(const Line &l, std::set<Line> &lines);
